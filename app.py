083b35a19081a9ae36619e527952b058e0bb8b5c
orlandoalcarraz <alcarraz_26@hotmail.com>
2024-12-07 03:54:58 -0500

    update project

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import os
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings 
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
 
 st.set_page_config('Ask PDF')
